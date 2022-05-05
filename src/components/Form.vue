1a4c46014d6fd10822ecf8f1142275369bf651ee
Pobx <geidtiphong@gmail.com>
2022-05-05 20:46:51 +0700

    use mapActions for involk more methods to component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,18 +2,32 @@
   <div>
     <label for="flavor">Favorite ice cream flavor?</label>
     <input type="text" name="flavor" @input="changed" />
+
+    <br />
+
+    <button type="button" @click="increment">+</button>&nbsp;
+    <button type="button" @click="decrement">-</button>&nbsp;
+    <button type="button" @click="incrementIfOdd">Increment if odd</button>&nbsp;
+    <button type="button" @click="incrementAsync">Increment async</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   methods: {
     changed: function (event) {
       const data = event.target.value;
       console.log(data);
 
-      this.$store.commit('change', data);
+      this.$store.commit("change", data);
     },
+    ...mapActions([
+      "increment",
+      "decrement",
+      "incrementIfOdd",
+      "incrementAsync",
+    ]),
   },
 };
 </script>
