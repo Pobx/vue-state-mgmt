12f55410961c88ba88fe6e4446b38b0263bb5a40
Pobx <geidtiphong@gmail.com>
2022-05-05 20:23:10 +0700

    create new files and try to use store object

---
+++ src/components/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <label for="flavor">Favorite ice cream flavor?</label>
+    <input type="text" name="flavor" @input="changed" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changed: function (event) {
+      const data = event.target.value;
+      console.log(data);
+
+      this.$store.commit('change', data);
+    },
+  },
+};
+</script>
