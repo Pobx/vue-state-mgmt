d9bcd1687917059b2a749a2a01a38f4daca0cd2c
Pobx <geidtiphong@gmail.com>
2022-05-05 20:45:46 +0700

    use mapGetters for computed

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <p>You chose {{ $store.getters.flavor }}</p>
+    <hr />
+    <p>Counter: {{ $store.state.count }} times, count is {{ evenOrOdd }}.</p>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: mapGetters(["evenOrOdd"]),
+};
+</script>
